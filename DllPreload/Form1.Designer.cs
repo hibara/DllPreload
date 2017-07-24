cbbc8559013f91f90f458e9d395d24359d6fd391
Hibara, Mitsuhiro <m@hibara.org>
2017-07-24 10:18:28 +0900

    initial commit

---
+++ DllPreload/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DllPreload
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.button1.Location = new System.Drawing.Point(382, 187);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "E&xit";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(185, 94);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(71, 12);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "Main Window";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.button1;
+      this.ClientSize = new System.Drawing.Size(469, 222);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Main Window";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+  }
+}
+
---
+++ DllPreload/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DllPreload
+{
+  static class Program
+  {
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool SetDllDirectory(string lpPathName);
+
+    /// <summary>
+    /// Here is the main entry point of the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+
+      SetDllDirectory("");
+
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+
+}
